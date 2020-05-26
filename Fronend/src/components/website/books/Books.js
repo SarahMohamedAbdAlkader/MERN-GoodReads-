84695f98d29693124a5899c88dfa4714fad7fa51
Isaac Wahba <58470366+isaac-wahba@users.noreply.github.com>
2020-05-26 12:51:59 +0200

    Book component added
    Without the Side bar, rating and shelve changing

--- Fronend/src/components/website/books/Books.js
+++ Fronend/src/components/website/books/Books.js
@@ -1,96 +1,202 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import ReactPaginate from 'react-paginate';
-import '../categories/Categories.css'
-import bg2 from '../../assests/bg2.png'
-import { Link } from 'react-router-dom'
-import './Books.css'
-const SERVER_URL = "http://localhost:5000"
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            postData: [],
-            offset: 0,
-            data: [],
-            perPage: 10,
+import React from "react";
+import "./Books.css";
+import BeautyStars from "beauty-stars";
+//import books from "../../Data.js";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+const API_URL = "http://localhost:5000";
+const options = [
+  { id: 1, value: "Read", label: "Read" },
+  { id: 2, value: "Reading", label: "Reading" },
+  { id: 3, value: "want to read", label: "want to read" },
+];
 
-            currentPage: 0
-        };
-        this.handlePageClick = this
-            .handlePageClick
-            .bind(this);
-    }
-    receivedData() {
-        axios
-            .get(`http://localhost:5000/books`)
-            .then(res => {
-
-                const data = res.data.books;
-                console.log(data,"dd",res.data.page)
-                const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
-                 this.state.postData = slice
-                 this.setState({postData:data})
-       
-                this.setState({
-                    pageCount: Math.ceil(res.data.dataLength / this.state.perPage),
-                })
-            
-        });
-            
-    }
-    handlePageClick = (e) => {
-        const selectedPage = e.selected;
-        const offset = selectedPage * this.state.perPage;
-
-        this.setState({
-            currentPage: selectedPage,
-            offset: offset,
-          
-        }, () => {
-            this.receivedData()
-        });
-
+export default class All extends React.Component {
+  constructor(props) {
+    super(props); //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = {
+      books: [],
+      postData: [],
+      perPage: 10,
+      currentPage: 0,
+      //selectedPage: 0,
     };
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
+  receivedData() {
+    //data
+    axios.get(`http://localhost:5000/books`).then((res) => {
+      // console.log(res.data.books);
+      const books = res.data.books;
+      console.log(res.data);
+      console.log(this.state.offset);
 
-    componentDidMount() {
-        this.receivedData()
-    }
-    render() { const {postData}=this.state
-        return (
-            <div>
-                <div class="row  " >
-                {
-                postData.map((book, index) =>
-              
-                <div key={index}  style={{ width: 250 }}>
-<img class="card-img-top    "  style={{ width: 250,height:250 }} src= {`${SERVER_URL}/${book.bookImage}`}/>
-      <Link  class=" font-italic "style={{fontSize:18 }} to={"/books/" + book._id}>{book.name}</Link>
-                <Link style={{fontSize:18 }} class=" font-italic mb-5" to={"/author/" + book.author._id}>{book.author.firstName}{book.author.lastName}</Link>
-  </div>
-    
-                 ) }
-                 
-                </div>
-                <div class=" d-flex justify-content-center">
-                    <ReactPaginate
-                        previousLabel={"prev"}
-                        nextLabel={"next"}
-                        breakLabel={"..."}
-                        breakClassName={"break-me"}
-                        pageCount={this.state.pageCount}
-                        marginPagesDisplayed={2}
-                        pageRangeDisplayed={5}
-                        onPageChange={this.handlePageClick}
-                        containerClassName={"pagination"}
-                        subContainerClassName={"pages pagination"}
-                        activeClassName={"active"} />
-                </div>
-                <img class ="col-12"src={bg2}/>
-            </div>
+      const slice = books.slice(
+        this.state.offset,
+        this.state.offset + this.state.perPage
+      );
+      this.postData = slice;
+      this.setState({ postData: books });
 
-        )
-        
-    }
-    
+      this.setState({
+        pageCount: Math.ceil(res.data.dataLength / this.state.perPage),
+      });
+    });
+  }
+  handlePageClick = (e) => {
+    console.log("hi");
+    const selectedPage = e.selected;
+    this.setState(
+      {
+        currentPage: selectedPage,
+      },
+      () => {
+        axios.get(`http://localhost:5000/books/?page=` + 2).then((res) => {
+          //convert to number
+          const books = res.data.books; //??
+
+          this.postData = books;
+          this.setState({ postData: books });
+        });
+      }
+    );
+  };
+
+  componentDidMount() {
+    this.receivedData();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="readingProgress">All books</h1>
+        <table id="books">
+          <thead>{this.renderTableHeader()}</thead>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>{" "}
+        <ReactPaginate
+          previousLabel={"<<"}
+          nextLabel={">>"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
+      </div>
+    );
+  }
+
+  renderTableData() {
+    const { postData } = this.state;
+    return postData.map((book, index) => {
+      const {
+        id,
+        bookImage,
+        name,
+        author,
+        totalRatingValue,
+        myRating,
+        shelve,
+      } = book.book; //destructuring
+      //console.log(book.book);
+
+      return (
+        <tr key={id}>
+          <td>
+            <img
+              src={`${API_URL}/${bookImage}`}
+              style={{ width: 15, height: 15 }}
+              alt={name}
+            />
+          </td>
+          <td>{name}</td>
+          <td>
+            {author.firstName} {author.lastName}
+          </td>
+
+          <td
+            onChange={() => {
+              console.log("changed");
+            }}
+          >
+            <BeautyStars size={20} value={totalRatingValue} />
+          </td>
+          <td onClick={(e) => console.log(e.target)}>
+            <BeautyStars
+              /* value={this.rate} */
+              onChange={(e) => {
+                this.renderRating(index);
+                console.log(book.id);
+                const editeId = book.id;
+                console.log(editeId);
+                postData.map((item, index) => {
+                  console.log(item);
+                  if (item.id === editeId) {
+                    item.myRating = e;
+                    // item.rating=item.value;
+                  }
+                  return item;
+                });
+              }}
+              //key={id}
+              value={myRating}
+              size={20}
+            />
+          </td>
+          <td>
+            <select>
+              {options.map((item, id) => {
+                return shelve === item.value ? (
+                  <option key={item.id} value={item.value} selected>
+                    {item.label}
+                  </option>
+                ) : (
+                  <option key={item.id} value={item.value}>
+                    {item.label}
+                  </option>
+                );
+              })}
+            </select>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  renderTableHeader() {
+    //  let header = Object.keys(this.state.postData[0]);
+    //return header.map((key, index) => {
+    return (
+      <tr>
+        <th>cover</th>
+        <th>Name</th>
+        <th>Author</th>
+        <th>Avg Rate</th>
+        <th>Rating</th>
+        <th>shelve</th>
+      </tr>
+    );
+    //});
+  }
+
+  // rate: 0,
+
+  renderRating(index) {
+    const { postData } = this.state;
+
+    const newBooks = postData.map((book, id) => {
+      if (index === id) {
+        return { ...book, totalRatingValue: this.state.rate };
+      }
+      return book;
+    });
+    this.setState({
+      books: newBooks,
+    });
+  }
 }
