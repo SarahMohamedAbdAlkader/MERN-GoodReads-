be8def045021f527e612865ba8e099f2c0beeae1
Isaac Wahba <isaac.wahba@hotmail.com>
2020-05-28 03:43:44 +0200

    Books tables and Side bar components added, with styles.

---
+++ Fronend/src/components/website/books/Books.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import Tables from "./table/Tables";
+import "./Books.css";
+import ReactPaginate from "react-paginate";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+const SERVER_URL = "http://localhost:5000";
+//import ReactPaginate from "react-paginate";
+const token = sessionStorage.getItem("userToken");
+export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shelveState: "all",
+      books: [],
+      postData: [],
+      perPage: 10,
+      currentPage: 1,
+      booksChanges: false,
+      offset: 0,
+    };
+  }
+  receivedData() {
+    //data
+    axios
+      .get(`${SERVER_URL}/books/` + token + "/" + this.state.shelveState)
+      .then((res) => {
+        // console.log("data received at books ==>", res.data.books);
+        const books = res.data.books;
+        //console.log("Received data assigned: ==> ", books);
+        //console.log(this.state.offset);
+        const slice = books.slice(
+          this.state.offset,
+          this.state.offset + this.state.perPage
+        );
+        this.postData = slice;
+        this.setState({ postData: books });
+        //console.log("Received data  sliced ==>", this.postData);
+
+        this.setState({
+          pageCount: Math.ceil(res.data.dataLength / this.state.perPage),
+        });
+      });
+  }
+  componentDidMount() {
+    this.receivedData();
+  }
+  handlePageClick = (e) => {
+    console.log("hi");
+    const selectedPage = e.selected;
+    this.setState(
+      {
+        currentPage: selectedPage,
+      },
+      () => {
+        axios
+          .get(
+            `${SERVER_URL}/books/${token}/${this.state.shelveState}/?page=` +
+              parseInt(selectedPage)
+          )
+          .then((res) => {
+            const books = res.data.books;
+
+            this.postData = books;
+            this.setState({ postData: books });
+          });
+      }
+    );
+  };
+  render() {
+    return (
+      <div className='container'>
+        <div className='sideBarContainer'>
+          <ul onClick={(e) => console.log(e.target)}>
+            <ul className='readingProgress'>
+              <Link onClick={this.renderAll}>All Books</Link>
+            </ul>
+            <ul className='readingProgress'>
+              <Link onClick={this.renderCurrent}>Currently Reading</Link>
+            </ul>
+            <ul className='readingProgress'>
+              <Link onClick={this.renderRead}>Read Books</Link>
+            </ul>
+            <ul className='readingProgress'>
+              <Link onClick={this.renderWant}>Want to Read</Link>
+            </ul>
+          </ul>
+        </div>
+        <div>
+          <hr />
+        </div>
+
+        <div className='tableContainer'>
+          <Tables
+            booksChanges={this.state.booksChanges}
+            books={this.state.books}
+            postData={this.state.postData}
+          />
+          <ReactPaginate
+            previousLabel={"<<"}
+            nextLabel={">>"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
+        </div>
+      </div>
+    );
+  }
+  renderAll = (e) => {
+    this.setState({ shelveState: "all" });
+    console.log(this.state.shelveState);
+    this.receivedData();
+  };
+  renderCurrent = (e) => {
+    this.setState({ shelveState: "Currently Reading" });
+    console.log(this.state.shelveState);
+    this.receivedData();
+  };
+  renderRead = (e) => {
+    this.setState({ shelveState: "Read" });
+    console.log(this.state.shelveState);
+    this.receivedData();
+  };
+  renderWant = (e) => {
+    this.setState({ shelveState: "Want To Read" });
+    console.log(this.state.shelveState);
+    this.receivedData();
+  };
+}
---
+++ Fronend/src/components/website/books/table/Tables.js
@@ -0,0 +1,178 @@
+import React from "react";
+import "./Tables.css";
+import ReactStars from "react-stars";
+import axios from "axios";
+const SERVER_URL = "http://localhost:5000";
+const options = [
+  { id: 1, value: "Read", label: "Read" },
+  { id: 2, value: "Reading", label: "Reading" },
+  { id: 3, value: "want to read", label: "want to read" },
+];
+const token = sessionStorage.getItem("userToken");
+let userRating = 0;
+
+export default class Tables extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      postData: [],
+      perPage: 10,
+      currentPage: 1,
+      // selectedPage: 0,
+      booksChanges: false,
+      offset: 0,
+    };
+    this.handlePageClick = this.handlePageClick.bind(this);
+    // console.log("PostData Passed ==>", props.postData);
+    this.state.books = props.books;
+    this.state.postData = props.postData;
+    console.log("prpos post", this.state.postData);
+    console.log("Books are here !!", this.state.books);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='readingProgress'>All books</h1>
+        <table id='books'>
+          <thead>{this.renderTableHeader()}</thead>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>{" "}
+      </div>
+    );
+  }
+
+  renderTableData() {
+    const { postData } = this.props;
+    return postData.map((book, index) => {
+      userRating = book.myRating;
+      const token = sessionStorage.getItem("userToken");
+
+      const {
+        _id,
+        bookImage,
+        name,
+        author,
+        totalRatingValue,
+        totalRatingCount,
+      } = book.book.book; //destructuring
+      const { myRating, myShelve } = book;
+
+      //console.log(shelve);
+      return (
+        <tr key={_id}>
+          <td>
+            <img
+              src={`${SERVER_URL}/${bookImage}`}
+              style={{ width: 15, height: 15 }}
+              alt={name}
+            />
+          </td>
+          <td>{name}</td>
+          <td>
+            {author.firstName} {author.lastName}
+          </td>
+
+          <td>
+            <div class='mt-2'>
+              <ReactStars
+                count={5}
+                edit={false}
+                size={18}
+                color1={"grey"}
+                color2={"yellow"}
+                value={totalRatingValue / totalRatingCount}
+              />
+            </div>
+          </td>
+
+          <td onClick={(e) => console.log(e.target)}>
+            <ReactStars
+              count={5}
+              edit={true}
+              onChange={(e) => handleRatingChange(e, _id)}
+              size={24}
+              color1={"grey"}
+              color2={"yellow"}
+              value={myRating}
+            />
+          </td>
+          <td>
+            <select onChange={(e) => handleSelectChange(e, _id)}>
+              <option value='' selected={myShelve === ""}></option>/>
+              <option value='Read' selected={myShelve === "Read"}>
+                Read
+              </option>
+              />
+              <option
+                value='Want To Read'
+                selected={myShelve === "Want To Read"}
+              >
+                Want To Read
+              </option>
+              />
+              <option
+                value='Currently Reading'
+                selected={myShelve === "Currently Reading"}
+              >
+                Currently Reading
+              </option>
+              />
+            </select>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  renderTableHeader() {
+    return (
+      <tr>
+        <th>cover</th>
+        <th>Name</th>
+        <th>Author</th>
+        <th>Avg Rate</th>
+        <th>Rating</th>
+        <th>shelve</th>
+      </tr>
+    );
+    //});
+  }
+}
+const handleRatingChange = (e, bookId) => {
+  const token = sessionStorage.getItem("userToken");
+  console.log(bookId);
+  console.log(e);
+  userRating = e;
+  console.log(userRating);
+  axios
+    .post("http://localhost:5000/ratings/" + token, {
+      value: userRating,
+      bookId,
+    })
+    .then((res) => {
+      console.log("success", res.data);
+    })
+    .catch(function (error) {
+      console.log("failed", error);
+    });
+};
+const handleSelectChange = (e, bookId) => {
+  console.log(bookId);
+  const value = e.target.value;
+  console.log(value);
+  const token = sessionStorage.getItem("userToken");
+  console.log(token);
+  axios
+    .post("http://localhost:5000/shelve/" + token, {
+      state: value,
+      bookId,
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch(function (error) {
+      console.log(" failed to post the shelve state", error);
+    });
+};
